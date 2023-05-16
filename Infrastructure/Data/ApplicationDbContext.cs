1abb4c9317861bf7675bb4ee55ec28fda6cf7d2f
MaheshSinghMadai <maheshmadai977@gmail.com>
2023-05-16 15:40:17 +0545

    ProjectSetup

---
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+
+        //    modelBuilder.Entity<Route>()
+        //    .HasMany(b => b.BusDetails)
+        //        .WithOne(r => r.Route);
+        //}
+
+    }
+}
---
+++ Infrastructure/Repository.cs
@@ -0,0 +1,61 @@
+﻿using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly ApplicationDbContext _db;
+        private DbSet<T> entities;
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            entities = _db.Set<T>();
+        }
+        public void Create(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            entities.Add(entity);
+            _db.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+
+            entities.Remove(entity);
+            _db.SaveChanges();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return entities.ToList();
+        }
+
+        public void SaveChanges()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+
+            _db.SaveChanges();
+        }
+    }
+}
